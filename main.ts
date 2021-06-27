c9c373b35e9743d166d0eacc0efa951a99d06e81
kichinosukey <kichinosuke.fukuhara@gmail.com>
2021-06-27 22:37:01 +0900

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,6 +19,34 @@ radio.onReceivedNumber(function (receivedNumber) {
         maqueen.motorStop(maqueen.Motors.All)
     }
 })
+namespace Receiver {
+
+//% blockId=device_dispatcher
+//% block="dispatcher %receivedNumber %pause %speed"
+export function dispatcher(receivedNumber: number, pause: number = 500, speed: number = 130): void
+{
+    if (receivedNumber == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, speed)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, speed)
+        basic.pause(pause)
+        maqueen.motorStop(maqueen.Motors.All)
+    } else if (receivedNumber == 1) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, speed)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, speed)
+        basic.pause(pause)
+        maqueen.motorStop(maqueen.Motors.All)
+    } else if (receivedNumber == 2) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, speed)
+        basic.pause(pause)
+        maqueen.motorStop(maqueen.Motors.All)
+    } else if (receivedNumber == 3) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, speed)
+        basic.pause(pause)
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+}
+
+}
 basic.forever(function () {
     radio.setGroup(1)
 })
