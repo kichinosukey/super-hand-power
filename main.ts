0e882a041f37d74fc77f16f38f9d5fae391115b2
kichinosukey <kichinosuke.fukuhara@gmail.com>
2021-07-17 21:25:09 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -5,21 +5,21 @@ namespace HandPower {
 export function receiver(groupNumber: number = 1, pause: number = 500, speed: number = 130): void
 {
     radio.setGroup(groupNumber)
-    if (radio.receiveNumber() == 0) {
+    if (radio.receiveNumber() == 1) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, speed)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, speed)
         basic.pause(pause)
         maqueen.motorStop(maqueen.Motors.All)
-    } else if (radio.receiveNumber() == 1) {
+    } else if (radio.receiveNumber() == 2) {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, speed)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, speed)
         basic.pause(pause)
         maqueen.motorStop(maqueen.Motors.All)
-    } else if (radio.receiveNumber() == 2) {
+    } else if (radio.receiveNumber() == 3) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, speed)
         basic.pause(pause)
         maqueen.motorStop(maqueen.Motors.All)
-    } else if (radio.receiveNumber() == 3) {
+    } else if (radio.receiveNumber() == 4) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, speed)
         basic.pause(pause)
         maqueen.motorStop(maqueen.Motors.All)
@@ -32,13 +32,13 @@ export function sender(groupNumber: number = 1): void
 {
     radio.setGroup(groupNumber)
     if (input.isGesture(Gesture.TiltRight)) {
-        radio.sendNumber(0)
-    } else if (input.isGesture(Gesture.TiltRight)) {
         radio.sendNumber(1)
-    } else if (input.isGesture(Gesture.LogoDown)) {
+    } else if (input.isGesture(Gesture.TiltRight)) {
         radio.sendNumber(2)
-    } else if (input.isGesture(Gesture.LogoUp)) {
+    } else if (input.isGesture(Gesture.LogoDown)) {
         radio.sendNumber(3)
+    } else if (input.isGesture(Gesture.LogoUp)) {
+        radio.sendNumber(4)
         }
     }
 }
