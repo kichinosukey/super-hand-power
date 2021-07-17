25fd0b66b76abfe588613ccbdff04e09eb8986d4
kichinosukey <kichinosuke.fukuhara@gmail.com>
2021-07-17 21:20:14 +0900

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -2,23 +2,24 @@ namespace HandPower {
 
 //% blockId=device_receiver
 //% block="receiver %receivedNumber %pause %speed"
-export function receiver(receivedNumber: number, pause: number = 500, speed: number = 130): void
+export function receiver(groupNumber: number = 1, pause: number = 500, speed: number = 130): void
 {
-    if (receivedNumber == 0) {
+    radio.setGroup(groupNumber)
+    if (radio.receiveNumber() == 0) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, speed)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, speed)
         basic.pause(pause)
         maqueen.motorStop(maqueen.Motors.All)
-    } else if (receivedNumber == 1) {
+    } else if (radio.receiveNumber() == 1) {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, speed)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, speed)
         basic.pause(pause)
         maqueen.motorStop(maqueen.Motors.All)
-    } else if (receivedNumber == 2) {
+    } else if (radio.receiveNumber() == 2) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, speed)
         basic.pause(pause)
         maqueen.motorStop(maqueen.Motors.All)
-    } else if (receivedNumber == 3) {
+    } else if (radio.receiveNumber() == 3) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, speed)
         basic.pause(pause)
         maqueen.motorStop(maqueen.Motors.All)
@@ -27,9 +28,9 @@ export function receiver(receivedNumber: number, pause: number = 500, speed: num
 
 //% blockId=device_sender
 //% block="sender %sendNumber"
-export function sender(sendNumber: number = 1): void
+export function sender(groupNumber: number = 1): void
 {
-    radio.setGroup(1)
+    radio.setGroup(groupNumber)
     if (input.isGesture(Gesture.TiltRight)) {
         radio.sendNumber(0)
     } else if (input.isGesture(Gesture.TiltRight)) {
