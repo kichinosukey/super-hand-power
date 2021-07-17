9cd540926ee2ca6ac081d60011a6e58e0f56cfa4
kichinosukey <kichinosuke.fukuhara@gmail.com>
2021-07-17 22:35:52 +0900

    receiver shownumber

--- main.ts
+++ main.ts
@@ -4,21 +4,22 @@ namespace HandPower {
 //% block="receiver %receivedNumber %pause %speed"
 export function receiver(receivedNumber: number, pause: number = 500, speed: number = 130): void
 {
-    if (receivedNumber == 0) {
+    basic.showNumber(receivedNumber)
+    if (receivedNumber == 1) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, speed)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, speed)
         basic.pause(pause)
         maqueen.motorStop(maqueen.Motors.All)
-    } else if (receivedNumber == 1) {
+    } else if (receivedNumber == 2) {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, speed)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, speed)
         basic.pause(pause)
         maqueen.motorStop(maqueen.Motors.All)
-    } else if (receivedNumber == 2) {
+    } else if (receivedNumber == 3) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, speed)
         basic.pause(pause)
         maqueen.motorStop(maqueen.Motors.All)
-    } else if (receivedNumber == 3) {
+    } else if (receivedNumber == 4) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, speed)
         basic.pause(pause)
         maqueen.motorStop(maqueen.Motors.All)
