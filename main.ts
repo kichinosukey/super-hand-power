b0971337794a8c165e2b1363ade5db1c620ac9f2
kichinosukey <kichinosuke.fukuhara@gmail.com>
2021-08-09 13:41:21 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -8,21 +8,21 @@ namespace HandPower {
 export function receiver(receivedNumber: number, pause: number = 500, speed: number = 130): void
 {
     basic.showNumber(receivedNumber)
-    if (receivedNumber == 1) {
+    if (receivedNumber == 3) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, speed)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, speed)
         basic.pause(pause)
         maqueen.motorStop(maqueen.Motors.All)
-    } else if (receivedNumber == 2) {
+    } else if (receivedNumber == 4) {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, speed)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, speed)
         basic.pause(pause)
         maqueen.motorStop(maqueen.Motors.All)
-    } else if (receivedNumber == 3) {
+    } else if (receivedNumber == 1) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, speed)
         basic.pause(pause)
         maqueen.motorStop(maqueen.Motors.All)
-    } else if (receivedNumber == 4) {
+    } else if (receivedNumber == 2) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, speed)
         basic.pause(pause)
         maqueen.motorStop(maqueen.Motors.All)
@@ -33,17 +33,17 @@ export function receiver(receivedNumber: number, pause: number = 500, speed: num
 //% block="sender %sendNumber"
 export function sender(sendNumber: number): void
 {
-    if (input.isGesture(Gesture.TiltRight)) {
+    if (input.isGesture(Gesture.LogoDown)) {
         sendNumber = 1
     }  
     if (input.buttonIsPressed(Button.A)) {
-        sendNumber = 2
-    }  
-    if (input.buttonIsPressed(Button.B)) {
         sendNumber = 3
     }  
-    if (input.buttonIsPressed(Button.AB)) {
+    if (input.buttonIsPressed(Button.B)) {
         sendNumber = 4
+    }  
+    if (input.buttonIsPressed(Button.AB)) {
+        sendNumber = 2
     }
     basic.showNumber(sendNumber)
     radio.sendNumber(sendNumber)
