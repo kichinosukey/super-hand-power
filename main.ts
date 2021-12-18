76bfb341388c41c37fba8d8190d35936fed45a55
kichinosukey <kichinosuke.fukuhara@gmail.com>
2021-12-18 13:15:44 +0900

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -31,8 +31,9 @@ export function receiver(receivedNumber: number, pause: number = 500, speed: num
 
 //% blockId=device_sender
 //% block="sender %sendNumber"
-export function sender(sendNumber: number): void
+export function sender(): void
 {
+    let sendNumber
     if (input.isGesture(Gesture.LogoDown)) {
         sendNumber = 1
     }  
